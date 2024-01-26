5bcf80ff629fd19c7d09f15e5b141e8c5562b950
todor943 <todorator@gmail.com>
2024-01-27 01:35:13 +0200

    first commit

---
+++ CharacterBody2D.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class CharacterBody2D : Godot.CharacterBody2D
+{	
+	
+	public float speed = 400f;
+
+	public Vector2 ScreenSize; // Size of the game window.
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var velocity = new Vector2(0,0);
+
+		if (Input.IsActionPressed("Right"))
+			velocity.X += speed;
+		if (Input.IsActionPressed("Left"))
+			velocity.X -= speed;
+		if (Input.IsActionPressed("Down"))
+			velocity.Y += speed;
+		if (Input.IsActionPressed("Up"))
+			velocity.Y -= speed;
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * speed;
+		}
+		velocity = velocity.Normalized() * speed;
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+	}
+}
