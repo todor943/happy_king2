f6a2c50c0b470bad133777024db6c08c05195061
todor943 <todorator@gmail.com>
2024-01-27 20:01:40 +0200

    game over screen

--- Scripts/ProgressBarTexture.cs
+++ Scripts/ProgressBarTexture.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class ProgressBarTexture : TextureProgressBar
 {
+	private bool isDead = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,6 +13,11 @@ public partial class ProgressBarTexture : TextureProgressBar
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(Value <= 0 && !isDead) {
+			GD.Print("u died!");
+			isDead = true;
+			// change scene to game over
+		}
 	}
 
 	public void _on_timer_timeout() {
