0dc7b6a57316239536a9ca83e6971d1919a85f8e
todor943 <todorator@gmail.com>
2024-01-27 19:43:33 +0200

    bananas and guard

---
+++ Scripts/GuardNode2D.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class GuardNode2D : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var animPlayer = GetNode<AnimationPlayer>("GuardAnimation");
+		animPlayer.Play("standing");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
